81cf5371ed840f7164f9728228a20b7cf1db5106
Jake Martin <jake.martin@relativity.com>
2019-08-21 09:54:18 -0500

    added more bread messages

--- BreadBot/SlackEndpoint.cs
+++ BreadBot/SlackEndpoint.cs
@@ -59,23 +59,25 @@ namespace BreadBot
 				log.LogInformation("Receieved app mention request type.");
 
 				var random = new Random();
-				var randomPhrase = random.Next(0, 8);
+				var key = random.Next(0, 10);
 
-				var botText = new List<string>
+				var botText = new Dictionary<int, string>
 				{
-					"Let's get this bread!",
-					"Let's yeet this wheat.",
-					"'There is not a thing that is more positive than bread.' – Fyodor Dostoevsky",
-					"'With bread all sorrows are less' - from Don Quixote",
-					"Rather a piece of bread with a happy heart than wealth with grief. – Egyptian Proverb",
-					"Check before you bite if it is bread or a stone. - Croatian Proverb",
-					"Let them eat cake! - Marie Antoinnette",
-					"'The sky is the daily bread of the eyes.' - Ralph Waldo Emerson"
+					{0, "Let's get this bread!"},
+					{1, "Let's yeet this wheat."},
+					{2, "There is not a thing that is more positive than bread. – Fyodor Dostoevsky"},
+					{3, "With bread all sorrows are less - from Don Quixote"},
+					{4, "Rather a piece of bread with a happy heart than wealth with grief. – Egyptian Proverb"},
+					{5, "Check before you bite if it is bread or a stone. - Croatian Proverb"},
+					{6, "Let them eat cake! - Marie Antoinnette"},
+					{7, "The sky is the daily bread of the eyes. - Ralph Waldo Emerson"},
+					{8, "Bread is the king of the table, and all else is merely the court that surrounds the king. - Louis Bromfield"},
+					{9, "Without bread all is misery.' - William Cobbett"},
 				};
 
 				var message = new PostMessageModel
 				{
-					text = botText[randomPhrase],
+					text = botText[key],
 					channel = channelName
 				};
 
